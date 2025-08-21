a4ce3c48b61f91d5c85ee1ad62facc66b8d5067a
RDLR <leon1989@outlook.com>
2025-08-20 20:08:25 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quintoRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Ruben Dario Leon",                         # Tu nombre
+    author_email="leon@outlook.com",                 # Tu correo electrónico
+    url="https://github.com/leon1989-git/QuintoRepo",     # URL del proyecto
+)
